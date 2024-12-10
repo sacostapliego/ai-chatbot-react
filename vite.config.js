d29a667c0bf50599e8253337dd0b80ece726b7d6
Steven <steven.acosta867@gmail.com>
2024-12-10 15:11:59 -0500

    render deployment changes

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 3000
+  }
 });
